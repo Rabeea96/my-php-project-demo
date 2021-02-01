47f4f9a8421ce9ec830444313c5b7ee70f310cd8
Rabeea Moussa <Rabeea96@hotmail.com>
2021-02-01 21:02:56 +0100

    added connection to remote mysql

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
-<?php 
+<?php
 
-echo 'This is my PHP project!';
+$servername = "sql7.freemysqlhosting.net";
+$username = "sql7390181";
+$password = "5fQWjdqkQz";
+$dbname = "sql7390181";
 
-?>
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?> 
