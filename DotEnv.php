e5311c7454d4e5c5a1461c3ee1ffb0e5c04b9500
Rabeea Moussa <Rabeea96@hotmail.com>
2021-02-07 01:51:24 +0100

    Update DotEnv.php

--- DotEnv.php
+++ DotEnv.php
@@ -1,17 +1,13 @@
 <?php
 
 class DotEnv {
-    /**
-     * The directory where the .env file can be located.
-     *
-     * @var string
-     */
+    
+    // The directory where the .env file can be located.
     protected $path;
 
 
     public function __construct(string $path) {
-
-        // When we are not on heroku
+        // When we are not on heroku - because when we are on a remote server like heroku, then the environment variables will exist on the server
         if (strpos($_SERVER['HTTP_HOST'], 'heroku') === FALSE) {
             if(!file_exists($path)) {
                 throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
@@ -21,7 +17,7 @@ class DotEnv {
     }
 
     public function load() :void {
-        // When we are not on heroku
+        // When we are not on heroku - because when we are on a remote server like heroku, then the environment variables will exist on the server
         if (strpos($_SERVER['HTTP_HOST'], 'heroku') === FALSE) {
             if (!is_readable($this->path)) {
                 throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
