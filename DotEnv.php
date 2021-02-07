e07d63e10a6f1e823a4712cbf10b5607690de05c
Rabeea Moussa <Rabeea96@hotmail.com>
2021-02-07 01:43:27 +0100

    Revert "Update DotEnv.php"
    This reverts commit ea22622ed71cbe9ee5d9e648dee2e1c923fd6f18.

--- DotEnv.php
+++ DotEnv.php
@@ -9,52 +9,38 @@ class DotEnv {
     protected $path;
 
 
-    public function __construct(string $path) {
-        // When we are not on heroku
-        if (strpos($_SERVER['HTTP_HOST'], 'heroku') === FALSE) {
-            if(!file_exists($path)) {
-                throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
-            }
+    public function __construct(string $path)
+    {
+        if(!file_exists($path)) {
+            throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
         }
-
         $this->path = $path;
     }
 
-    public function load() :void {
-
-        // When we are not on heroku
-        if (strpos($_SERVER['HTTP_HOST'], 'heroku') === FALSE) {
-            if (!is_readable($this->path)) {
-                throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
-            }
-
-            $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            foreach ($lines as $line) {
-
-                if (strpos(trim($line), '#') === 0) {
-                    continue;
-                }
-
-                list($name, $value) = explode('=', $line, 2);
-                $name = trim($name);
-                $value = trim($value);
-
-                if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
-                    putenv(sprintf('%s=%s', $name, $value));
-                    $_ENV[$name] = $value;
-                    $_SERVER[$name] = $value;
-                }
-            }
+    public function load() :void
+    {
+        if (!is_readable($this->path)) {
+            throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
         }
-        else {
-            // When we are on heroku, we load the .env variables by using the prefix "process.env."
-            putenv('DATABASE_SERVER', process.env.DATABASE_SERVER);
-            putenv('DATABASE_USER', process.env.DATABASE_USER);
-            putenv('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD);
-            putenv('DATABASE_NAME', process.env.DATABASE_NAME);
+
+        $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
         }
     }
-
 }
 
 ?>
